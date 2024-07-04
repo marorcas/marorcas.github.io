69f787c3bd3d5ccd80ff849858fbbfbf569f045d
marorcas <ortzmc@gmail.com>
2024-07-04 13:32:53 +1000

    Create project modal

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-document.getElementById('current-year').textContent = new Date().getFullYear();
+document.getElementById('enter').addEventListener('click', function() {
+    window.location.href = './portfolio.html';
+});
+
+
+//open and close project modal
+const projects = document.querySelectorAll('.content__projects__icon');
+const projectModal = document.getElementById('projectModal');
+
+projects.forEach((project) => {
+    project.addEventListener('click', () => {
+        projectModal.style.display = 'flex';
+    });
+});
+
+const closeProject = document.getElementById('closeProject');
+
+closeProject.addEventListener('click', () => {
+    projectModal.style.display = 'none';
+});
--- script.js
+++
@@ -1,3 +0,0 @@
-document.getElementById('enter').addEventListener('click', function() {
-    window.location.href = './portfolio.html';
-});
