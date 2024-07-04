227eb0f47ea2919531e930a9aaffb3b6d90ca0fe
marorcas <ortzmc@gmail.com>
2024-07-04 14:00:40 +1000

    Save changes to projects

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 document.getElementById('enter').addEventListener('click', function() {
     window.location.href = './portfolio.html';
 });
+
+//open and close project modal
+const projects = document.querySelectorAll('.content__projects__icon');
+const projectModal = document.getElementById('projectModal');
+
+projects.forEach((project) => {
+    project.addEventListener('click', () => {
+        projectModal.style.display = 'flex';
+    });
+});
+
+const closeProject = document.getElementById('closeProject');
+
+closeProject.addEventListener('click', () => {
+    projectModal.style.display = 'none';
+});
