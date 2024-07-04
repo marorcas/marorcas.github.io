9784f007defbf3af2148913871d7f9ecc210c99b
marorcas <ortzmc@gmail.com>
2024-07-04 15:44:17 +1000

    Add working project modal

---
+++ js/index.js
@@ -0,0 +1,4 @@
+const enter = document.getElementById('enter');
+enter.addEventListener('click', function() {
+    window.location.href = './portfolio.html';
+});
--- js/portfolio.js
+++ js/portfolio.js
@@ -1,19 +1,16 @@
-document.getElementById('enter').addEventListener('click', function() {
-    window.location.href = './portfolio.html';
-});
+// document.getElementById('current-year').textContent = new Date().getFullYear();
 
 //open and close project modal
 const projects = document.querySelectorAll('.content__projects__icon');
 const projectModal = document.getElementById('projectModal');
-
-projects.forEach((project) => {
+[...projects].forEach((project) => {
     project.addEventListener('click', () => {
+        console.log(project);
         projectModal.style.display = 'flex';
     });
 });
 
 const closeProject = document.getElementById('closeProject');
-
 closeProject.addEventListener('click', () => {
     projectModal.style.display = 'none';
 });
