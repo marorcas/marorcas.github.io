a6af2f6df9b6ef6e3a8d0d6a03273be573454850
marorcas <ortzmc@gmail.com>
2026-04-10 19:00:39 +1000

    creating js file for projects

---
+++ src/data/projects.js
@@ -0,0 +1,23 @@
+export const projects = [
+  {
+    id: "001",
+    title: "project 1",
+    description: "description 1",
+    stack: ["tool 1", "tool 2"],
+    demo: "#"
+  },
+  {
+    id: "002",
+    title: "project 2",
+    description: "description 2",
+    stack: ["tool 1", "tool 2"],
+    demo: "#"
+  },
+    {
+    id: "003",
+    title: "project 3",
+    description: "description 3",
+    stack: ["tool 1", "tool 2"],
+    demo: "#"
+  }
+];
